7241aefa5df25d122baeba35b48ca838c2ff7064
tabrez <04tabrez@gmail.com>
2025-09-24 23:03:52 +0530

    feat : change in folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-7xl font-bold font-mono '>SIH RAG</h1>
+    </div>
+  )
+}
+
+export default App
